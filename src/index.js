756c15caddd5b0dd8ca53700d0eb27a6e879d409
prakhar <prakharharshit.2003@gmail.com>
2024-04-02 16:06:36 +0530

    added notification ticket model

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 const express=require('express');
 const bodyparser=require('body-parser');
 const {PORT}=require('./config/serverconfig');
-const {sendBasicEmail}=require('./services/email-service');
+// const {sendBasicEmail}=require('./services/email-service');
 
 const bodyParser = require('body-parser');
-
+const cron=require('node-cron');
 const setupAndStartServer=()=>{
     const app=express();
 
@@ -15,13 +15,20 @@ const setupAndStartServer=()=>{
     );
     app.listen(PORT,()=>{
 console.log(`Server started at Port ${PORT}`);
-sendBasicEmail(
-    'support@admin.com',
-    'developerxnew@gmail.com',
-    'This is a texting email',
-    'Hey,how are you,I hope u like the support'
-);
+// sendBasicEmail(
+//     'support@admin.com',
+//     'developerxnew@gmail.com',
+//     'This is a texting email',
+//     'Hey,how are you,I hope u like the support'
+// );
+cron.schedule('*/2 * * * *', () => {
+    console.log('running a task every two minutes');
+  });
+
 
     });
 }
 setupAndStartServer();
+
+
+
