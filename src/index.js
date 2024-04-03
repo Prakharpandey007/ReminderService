24df482a1faf64be48263e396ae41431bfca0095
prakhar <prakharharshit.2003@gmail.com>
2024-04-03 20:20:34 +0530

    added rabbit mq

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ const express=require('express');
 const bodyparser=require('body-parser');
 const {PORT}=require('./config/serverconfig');
 // const {sendBasicEmail}=require('./services/email-service');
-
+// const {createChanel}=require('./utils/messageQueue')
 const bodyParser = require('body-parser');
 // const { Utils } = require('sequelize');
 // const cron=require('node-cron');
-const jobs=require('./utils/job');
+// const jobs=require('./utils/job');
 const TicketController=require('./controllers/ticket-controller');
-const setupAndStartServer=()=>{
+const setupAndStartServer=async ()=>{
     const app=express();
 
     app.use(bodyParser.json());
@@ -17,9 +17,12 @@ const setupAndStartServer=()=>{
     })
     );
     app.post('/api/v1/ticket',TicketController.create);
+    // const channel=await createChanel();
+    // subscribeMessage(channel, EmailService.subscribeEvents, REMINDER_BINDING_KEY);
+    
     app.listen(PORT,()=>{
 console.log(`Server started at Port ${PORT}`);
-jobs();
+// jobs();
 
 // sendBasicEmail(
 //     'support@admin.com',
